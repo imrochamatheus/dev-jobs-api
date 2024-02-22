4646987d9ee545fca0e6d746ef3141ef0262ac80
Matheus Rocha <im.rochamatheus@gmail.com>
2024-02-21 23:14:59 -0300

    refactor: move server definitions to configs dir

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,11 +1,13 @@
 import "express-async-errors";
 import express from "express";
+import morgan from "morgan";
 
-import ROUTES from "./routes";
-import {errorHandlerMiddleware} from "./middlewares/errorHandler.middleware";
+import ROUTES from "../routes";
+import {errorHandlerMiddleware} from "../middlewares/errorHandler.middleware";
 
 const SERVER = express();
 
+SERVER.use(morgan("dev"));
 SERVER.use(express.json());
 SERVER.use("/api", ROUTES);
 SERVER.use(errorHandlerMiddleware);
