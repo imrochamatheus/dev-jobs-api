8bae565380675fc649c501370263a7ebd7f9018d
Matheus Rocha <im.rochamatheus@gmail.com>
2024-02-23 19:28:02 -0300

    feat: add company route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,14 @@ import {Router} from "express";
 import {USER_ROUTER} from "./users";
 import {JOBS_ROUTER} from "./jobs";
 import {AUTH_ROUTER} from "./auth";
+import {COMPANY_ROUTER} from "./company";
 import {authMiddleware} from "../middlewares/auth.middleware";
 
 const ROUTES = Router();
 
 ROUTES.use("/login", AUTH_ROUTER);
-ROUTES.use("/jobs", [authMiddleware], JOBS_ROUTER);
 ROUTES.use("/users", USER_ROUTER);
+ROUTES.use("/jobs", [authMiddleware], JOBS_ROUTER);
+ROUTES.use("/company", [authMiddleware], COMPANY_ROUTER);
 
 export default ROUTES;
